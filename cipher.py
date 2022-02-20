4e88c16b76e385846c1e95455e7325bc820d71de
Sarah Branch <sarah@Sarahs-MacBook-Pro.local>
2022-02-19 23:26:11 -0500

    I read over my retro and removed any extra comments in cipher.py.

--- cipher.py
+++ cipher.py
@@ -1,21 +1,9 @@
+#imports
 import random #for random.randrange() function 
 import time
-'''
-Code
-- reads a file and its pad to decrypt a message
-- can encrypt a message with a pad 
 
-'''
 
-'''
-newfile = time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime())
-message = input("Please input a message: ")
-askpad = input("Please input 0 to generate a pad or 1 to to ender your own pad: ")
-if askpad == '1':
-       pad = generatePad(message)
-elif askpad == '0':
-    pad = input("Please input the pad: ")
-'''
+
 #ask the users if they want to encipher or decipher 
 def ask():
     askCipher = input("Would you like to encrypt or decrypt? Type 0 for encrypt or 1 for decrypt ")
@@ -28,9 +16,12 @@ def ask():
         ask()
     
 
+
 #makes string a list of letters/characters
 def getChar(message):
     return [letter for letter in message]
+
+
 
 #reads the file the user inputs
 def readFile(filename):
@@ -50,11 +41,10 @@ def writePad(pad):
         file.write(pad)
         file.close()
 
-#takes the lenof the message to make a pad of that length for the user to encrypt with
+#takes the length of the message to make a pad of that length for the user to encrypt with
 def generatePad(message):
-    #message = readFile(message)
     padNum = len(message)  
-    pad = []
+    pad = [] #empty list to put items in for pad
     while padNum != 0:
         num = random.randrange(65, 90, 1) #following capital letters for ascii code
         num = chr(num)
@@ -68,26 +58,17 @@ def generatePad(message):
 
 
 
-
 #deciphers the message by puttng the pad and message in a string, then list, and adding them together (had to have one to add the message differently if letters in message are uppercase or lowercase and same with the pad)
 def decipher(message, pad): 
-    #message = input("Please input the message you would like to decipher: ")
-    #pad = input("Please input the pad: ")
-    #message = readFile(message)
-    #pad = readFile(pad)
-
     message = getChar(message)
     pad = getChar(pad)
     
-    #print(message)
     for i in range(len(pad)): 
         pad[i] = ord(pad[i])
-        #print(type(pad[i]))
     
-    x = 0
+    x = 0 #index for pad
     for i in range(len(message)): 
         message[i] = ord(message[i])
-        #print(type(message[i]))
 
         if (message[i] >= 65) and (message[i] <= 90):
             if (pad[x] >= 65) and (pad[x] <= 90):
@@ -96,7 +77,7 @@ def decipher(message, pad):
             elif (pad[x] >= 97) and (pad[x] <= 122):
                 message[i] = ((message[i] - 65) - (pad[x] - 97)) % 26 + 65
             
-            x += 1
+            x += 1 #increment pad by one
 
         elif (message[i] >= 97) and (message[i] <= 122):
             if (pad[x] >= 65) and (pad[x] <= 90):
@@ -106,84 +87,59 @@ def decipher(message, pad):
                 message[i] = ((message[i] - 97) - (pad[x] - 97)) % 26 + 97
 
             x += 1 # increases pad by 1 separately from the message incase pad is longer than the message
-
-        
+    
     for a in range(len(message)): 
         message[a] = chr(message[a])
-    #print(message)
-    final_phrase = "".join(str(item) for item in message)
+    
+    final_phrase = "".join(str(item) for item in message) #makes the list into a string
     print("The decrypted message is: \n" + final_phrase + "\n")
     writeMessage(final_phrase)
     return final_phrase
 
+
+
 #encrypts the message by adding the ascii code pad woth the ascii code message
 def encipher(message, pad):
-    #message = input("Please input the file you would like to encode: ")
-    #message = readFile(message)
-    #askpad = input("Would you like to input a pad or make your own? Type 1 to make a pad or 0 to input your own ")
-    #if askpad == '1':
-       #newpad = generatePad(message)
-    #elif askpad == '0':
-        #newpad = input("Please input file path to pad: ")
-    #pad = readFile(pad)
-    #print(message)
-    #print(newpad)
-
     message = getChar(message)
     pad = getChar(pad)
     
-    #print(message)
     for i in range(len(pad)): 
         pad[i] = ord(pad[i])
-        #print(type(pad[i]))
     
-    x = 0
+    x = 0 #index for pad
     for i in range(len(message)): 
         message[i] = ord(message[i])
-        #print(type(message[i]))
 
         if (message[i] >= 65) and (message[i] <= 90):
             if (pad[x] >= 65) and (pad[x] <= 90):
-                message[i] = ((message[i] - 65) + (pad[x] - 65)) % 26 + 65 #need
+                message[i] = ((message[i] - 65) + (pad[x] - 65)) % 26 + 65 
                 
             elif (pad[x] >= 97) and (pad[x] <= 122):
                 message[i] = ((message[i] - 65) + (pad[x] - 97)) % 26 + 65
-            x += 1
+            x += 1 #increment pad by 1
 
         elif (message[i] >= 97) and (message[i] <= 122):
             if (pad[x] >= 65) and (pad[x] <= 90):
-                message[i] = ((message[i] - 97) + (pad[x] - 65)) % 26 + 97 #the lowercase letters are off by 6
-                #x += 1
+                message[i] = ((message[i] - 97) + (pad[x] - 65)) % 26 + 97 
+                
             elif (pad[x] >= 97) and (pad[x] <= 122):
                 message[i] = ((message[i] - 97) + (pad[x] - 97)) % 26 + 97
-            x += 1
+            x += 1 #increment pad by 1
 
         
     for a in range(len(message)): 
-        message[a] = chr(message[a])
-    #print(message)
-    final_phrase = "".join(str(item) for item in message)
-    #writeMessage(final_phrase)
+        message[a] = chr(message[a]) #makes ascii code back to character
+    
+    final_phrase = "".join(str(item) for item in message) #makes list into string
+    
     print("The encrypted message is: \n" + final_phrase + "\n")
     writeMessage(final_phrase)
     return final_phrase
     
-#runs ask() function to run the rest of the programs/functions/prompt user
-#ask()
-#decipher(message, pad)
-#print(type(decipher("encrypted-message.txt", "pad.txt")))
 
-newfile = time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime())
-#message = input("Please input a message: ")
-#message = readFile(message)
-#askpad = input("Please input 1 to generate a pad or 0 to to enter your own pad: ")
-#if askpad == '1':
-       #pad = generatePad(message)
-#elif askpad == '0':
-    #pad = input("Please input the pad: ")
-    #pad = readFile(pad)
 
-#encipher(message, pad)
+newfile = time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime()) #to add time to the filename for the message and pad
+
 
 '''
 Links/ Resources Used
@@ -195,6 +151,9 @@ joining sentence together: https://www.simplilearn.com/tutorials/python-tutorial
 went to professor office hours to ask if input should be a file or string 
 creating a new file to store message: https://stackoverflow.com/questions/31793549/new-file-name-each-time-python-run m
 zip(): https://www.geeksforgeeks.org/python-iterate-multiple-lists-simultaneously/ 
+went to office hours to ask about input (if they had to be filename or the actual message)
+Yanning's TA Hours to ask about why pand and message only wrote in one file (suggested to make 2 write functions, one for the pad and one for the meessage)
+went to Waka's TA hours to ask about test cases for encipher and decipher function and ask about the nputs 
 
 
 '''
